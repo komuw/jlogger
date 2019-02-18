68c513a9b86faf4e65dbdb0ffa95011c12b2b657
komuW <komuw05@gmail.com>
2019-02-18 03:40:26 +0300

    init

---
+++ src/main/java/engineer/komu/logger/LoggerApp.java
@@ -0,0 +1,19 @@
+/*-
+run this as:
+  export CLASSPATH=/Users/komuw/mystuff/jlogger/src/main/java && \
+  javac engineer/komu/logger/LoggerApp.java && \
+  java engineer/komu/logger/LoggerApp
+  
+  or
+  java engineer.komu.logger.LoggerApp
+
+*/
+package engineer.komu.logger;
+
+public class LoggerApp {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("LoggerApp");
+        System.out.println();
+    }
+}
